d632c9f3d9fd8f81b4945b269e250a6903503e46
Marisaliap <pascal.marisa@gmail.com>
2021-08-22 00:53:24 -0300

    practico JS IV

---
+++ 05-JS-IV/homework/practico_objetos.js
@@ -0,0 +1,29 @@
+var alumnos = [
+    { nombre : "Emi",
+     edad : 23,
+     hobbies : ["correr", "dormir", "nadar"],
+     amigos : [
+       {
+         nombre: "Martín",
+       },
+        {
+         nombre: "Leo",
+       }
+     ],
+  },
+  { nombre : "Franco",
+     edad : 21,
+     hobbies : ["fútbol", "golf"],
+     amigos : [
+       {
+         nombre: "Emi",
+       },
+        {
+         nombre: "Jimmy",
+       },
+        {
+         nombre: "Matías",
+       }
+     ],
+  },   
+  ];
